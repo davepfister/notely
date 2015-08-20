9f5ce9efb409d45bfbba9e2e63a79aae810b6826
Dave Pfister <davepfister@comcast.net>
2015-08-20 14:03:49 -0400

    Post new notes from a form to the backend

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -9,26 +9,23 @@ angular.module('notely.notes', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/notes', {
     templateUrl: 'notes/notes.html'
-  //  controller: 'NotesController'
   });
 }])
 
 .controller('NotesController', ['$scope', '$http', function($scope, $http) {
+  $scope.note = {};
+
   $http.get(nevernoteBasePath + 'notes?api_key=' + apiKey)
     .success(function(notesData) {
       $scope.notes = notesData;
     });
 
-    $scope.commit = function() {
-      $http.post(nevernoteBasePath + 'notes', {
-        api_key: apiKey,
-        note: {
-          title: 'The magic of AngularJS',
-          body_html: 'Whoever wrote this API must be a person'
-        }
-      }).success(function(newNoteData) {
-        console.log('Saved:');
-        console.log(newNoteData);
-      });
-    };
+  $scope.commit = function() {
+    $http.post(nevernoteBasePath + 'notes', {
+      api_key: apiKey,
+      note: $scope.note
+    }).success(function(newNoteData) {
+      $scope.notes.unshift(newNoteData.note);
+    });
+  };
 }]);

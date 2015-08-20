84d9bca3211d63e3d04df29ba349f56db6b1612b
Dave Pfister <davepfister@comcast.net>
2015-08-20 17:16:30 -0400

    sidebar and load existing note to form

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -3,29 +3,47 @@
 var nevernoteBasePath = 'https://nevernote-1150.herokuapp.com/api/v1/',
   apiKey = '$2a$10$d7XzSnDXgHoSKu4jFCs6v.aLSTJep.syPcZo1RFTm6w4xNZSdvUyq';
 
-//define angular module
-angular.module('notely.notes', ['ngRoute'])
+  var noteApp = angular.module('notely.notes', ['ngRoute']);
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/notes', {
-    templateUrl: 'notes/notes.html'
-  });
-}])
-
-.controller('NotesController', ['$scope', '$http', function($scope, $http) {
-  $scope.note = {};
-
-  $http.get(nevernoteBasePath + 'notes?api_key=' + apiKey)
-    .success(function(notesData) {
-      $scope.notes = notesData;
+  noteApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/notes', {
+      templateUrl: 'notes/notes.html'
     });
+  }]);
 
-  $scope.commit = function() {
-    $http.post(nevernoteBasePath + 'notes', {
-      api_key: apiKey,
-      note: $scope.note
-    }).success(function(newNoteData) {
-      $scope.notes.unshift(newNoteData.note);
-    });
-  };
-}]);
+  noteApp.controller('NotesController', ['$scope', 'NotesBackend', function($scope, NotesBackend) {
+    var self = this;
+    $scope.note = {};
+    $scope.notes = [];
+
+    self.assignNotes = function(notes) {
+      $scope.notes = notes;
+      //debugger;
+    };
+
+    self.findNoteById = function (noteId) {
+      for (var i = 0; i < $scope.notes.length; i++) {
+        if ($scope.notes[i].id === noteId){
+          return $scope.notes[i];
+        }
+      }
+    };
+
+    self.cloneNote = function(note) {
+      return JSON.parse(JSON.stringify(note));
+    };
+
+    $scope.commit = function() {
+      NotesBackend.postNote($scope.note, self.assignNotes);
+    };
+
+    $scope.hasNotes = function() {
+      return $scope.notes.length > 0;
+    };
+
+    $scope.loadNote = function(note) {
+      $scope.note =  self.cloneNote(note);
+    };
+
+    NotesBackend.fetchNotes(self.assignNotes);
+  }]);
